96f9c0423c7df84b149a657aa37bf8b7976b52fa
xuyanwei <xuyanwei@CNBJWK5172G6V.letv.local>
2015-10-23 12:04:52 +0800

    1.change zookeeper lock mode

--- src/api/common/utils/monitor_backend_handle_worker.py
+++ src/api/common/utils/monitor_backend_handle_worker.py
@@ -18,23 +18,23 @@ class Monitor_Backend_Handle_Worker(object):
         super(Monitor_Backend_Handle_Worker,self).__init__()
         
     def run(self):
+        '''
+            if no logic below, singleton Scheduler_ZkOpers may have no self.zk object.
+        '''
+        zk_addr, zk_port = local_get_zk_address()
+        if not (zk_addr and zk_port):
+            return
+            
+        zkOper = Scheduler_ZkOpers()
+        logging.info('check zk is connected :%s' % str(zkOper.is_connected()))
+        leader_flag = check_leader(zkOper)
+        if leader_flag == False:
+            logging.info("This node is not the leader of zookeeper, give up this chance")
+            return
+            
+        logging.info("This node is leader of zookeeper.")
+            
         try:
-            '''
-                if no logic below, singleton Scheduler_ZkOpers may have no self.zk object.
-            '''
-            zk_addr, zk_port = local_get_zk_address()
-            if not (zk_addr and zk_port):
-                return
-            
-            zkOper = Scheduler_ZkOpers()
-            logging.info('check zk is connected :%s' % str(zkOper.is_connected()))
-            leader_flag = check_leader(zkOper)
-            if leader_flag == False:
-                logging.info("This node is not the leader of zookeeper, give up this chance")
-                return
-            
-            logging.info("This node is leader of zookeeper.")
-            
             isLock, lock = zkOper.lock_async_monitor_action()
             if not isLock:
                 raise CommonException('a thread is running the monitor async, give up this oper on this machine!')
