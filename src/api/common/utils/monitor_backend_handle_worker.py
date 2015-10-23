bc3388c5e7a281088b18e0057f878639656fb91b
xuyanwei <xuyanwei@CNBJWK5172G6V.letv.local>
2015-10-23 11:23:45 +0800

    1.change zookeeper lock mode

--- src/api/common/utils/monitor_backend_handle_worker.py
+++ src/api/common/utils/monitor_backend_handle_worker.py
@@ -27,24 +27,26 @@ class Monitor_Backend_Handle_Worker(object):
                 return
             
             zkOper = Scheduler_ZkOpers()
-            logging.info('check zk is connected :%s' % str(zkOper.is_connected()) )
-            isLock, lock = zkOper.lock_async_monitor_action()
+            logging.info('check zk is connected :%s' % str(zkOper.is_connected()))
             leader_flag = check_leader(zkOper)
             if leader_flag == False:
                 logging.info("This node is not the leader of zookeeper, give up this chance")
                 return
-            logging.info("This node is leader of zookeeper.")
-        except kazoo.exceptions.LockTimeout:
-            logging.info("a thread is running the monitor async, give up this oper on this machine!")
-            return
             
-        if not isLock:
-            raise CommonException('a thread is running the monitor async, give up this oper on this machine!')
-        
-        try:
+            logging.info("This node is leader of zookeeper.")
+            
+            isLock, lock = zkOper.lock_async_monitor_action()
+            if not isLock:
+                raise CommonException('a thread is running the monitor async, give up this oper on this machine!')
+            
             data_node_info_list = zkOper.retrieve_data_node_list()
             self.__action_monitor_async(data_node_info_list)
             logging.info('this monitoring is over~')
+            
+        except kazoo.exceptions.LockTimeout:
+            logging.info("a thread is running the monitor async, give up this oper on this machine!")
+            return
+        
         finally:
             if lock is not None:
                 zkOper.unLock_aysnc_monitor_action(lock)
