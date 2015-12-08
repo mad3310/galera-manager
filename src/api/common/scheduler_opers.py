f13b2fce4a195a59afc8597eef41858f669de674
xuyanwei <xuyanwei@CNBJWK5172G6V.letv.local>
2015-12-08 17:07:18 +0800

    1.fix bug

--- src/api/common/scheduler_opers.py
+++ src/api/common/scheduler_opers.py
@@ -26,8 +26,9 @@ class Scheduler_Opers(object):
         '''
         self.thread_exception_hanlder(5)
         self.sced_monitor_handler(55)
+        self.check_db_wr_available(155)
         self.check_db_anti_item(195)
-        self.check_db_wr_available(295)       
+            
         
     def sced_monitor_handler(self, action_timeout = 30):
         # Create a periodic callback that tries to access async monitor interface
