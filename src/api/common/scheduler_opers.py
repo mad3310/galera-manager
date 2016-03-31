721f38cac5c152a3668d168ad90eeca9171412ad
xuyanwei <xuyanwei@CNBJWK5172G6V.letv.local>
2016-03-31 19:50:45 +0800

    1.refactor monitor code

--- src/api/common/scheduler_opers.py
+++ src/api/common/scheduler_opers.py
@@ -1,13 +1,12 @@
 #-*- coding: utf-8 -*-
 import sys
-
 from tornado.ioloop import PeriodicCallback
 from common.utils.threading_exception_handle_worker import Thread_Exception_Handler_Worker
 from common.utils.monitor_backend_handle_worker import Monitor_Backend_Handle_Worker
 from common.utils.monitor_db_anti_item_worker import Monitor_Db_Anti_Item
 from common.utils.threading_exception_queue import Threading_Exception_Queue
 from common.utils.monitor_inner_db_wr_worker import Monitor_Db_Wr_Available
-
+from common.utils.exceptions import CommonException
 '''
 Created on 2013-7-21
 
@@ -39,10 +38,12 @@ class Scheduler_Opers(object):
             except Exception:
                 self.threading_exception_queue.put(sys.exc_info())
 
-        if action_timeout > 0:
-            _monitor_async_t = PeriodicCallback(__create_worker_check_monitor,
+        if action_timeout < 0:
+            raise CommonException('timeout cannot be negative!')
+        
+        _monitor_async_t = PeriodicCallback(__create_worker_check_monitor,
                 action_timeout * 1000)
-            _monitor_async_t.start()
+        _monitor_async_t.start()
     
     def check_db_anti_item(self, action_timeout):
          
@@ -53,9 +54,11 @@ class Scheduler_Opers(object):
             except Exception:
                 self.threading_exception_queue.put(sys.exc_info())
                  
-        if action_timeout > 0:
-            _anti_async_t = PeriodicCallback(__check_db_anti, action_timeout * 1000)
-            _anti_async_t.start()
+        if action_timeout < 0:
+            raise CommonException('timeout cannot be negative!')
+        
+        _anti_async_t = PeriodicCallback(__check_db_anti, action_timeout * 1000)
+        _anti_async_t.start()
   
     def check_db_wr_available(self, action_timeout):
          
@@ -66,10 +69,12 @@ class Scheduler_Opers(object):
             except Exception:
                 self.threading_exception_queue.put(sys.exc_info())
                  
-        if action_timeout > 0:
-            _monitor_async_t = PeriodicCallback(__check_inner_db_wr_available,
+        if action_timeout < 0:
+            raise CommonException('timeout cannot be negative!')
+
+        _monitor_async_t = PeriodicCallback(__check_inner_db_wr_available,
             action_timeout * 1000)
-            _monitor_async_t.start()
+        _monitor_async_t.start()
            
     def thread_exception_hanlder(self, action_timeout = 5):
         if action_timeout > 0:
--- src/api/common/utils/monitor_backend_handle_worker.py
+++ src/api/common/utils/monitor_backend_handle_worker.py
@@ -1,16 +1,12 @@
 import logging
 import kazoo
 import time
-from handlers.monitor import Node_Info_Async_Handler, DB_Info_Async_Handler
 from common.zkOpers import Scheduler_ZkOpers
 from common.abstract_mysql_service_action_thread import Abstract_Mysql_Service_Action_Thread
+from common.utils.monitor_utils import Monitor_handle_Asyc
 
 
 class Monitor_Backend_Handle_Worker(Abstract_Mysql_Service_Action_Thread):
-  
-    node_handler = Node_Info_Async_Handler()
-    
-    db_handler = DB_Info_Async_Handler()
 
     def __init__(self, timeout):
         super(Monitor_Backend_Handle_Worker,self).__init__()
@@ -21,13 +17,14 @@ class Monitor_Backend_Handle_Worker(Abstract_Mysql_Service_Action_Thread):
             if no logic below, singleton Scheduler_ZkOpers may have no self.zk object.
         ''' 
         begin_time = time.time()
+        lock_name = "async_monitor/async_monitor_handler"
         
         zkOper = Scheduler_ZkOpers()
         logging.info('check zk is connected :%s' % str(zkOper.is_connected()))
-
+    
         isLock, lock = None, None
         try:
-            isLock, lock = zkOper.lock_async_monitor_handler_action()
+            isLock, lock = zkOper.lock_async_monitor_action(lock_name)
             if not isLock:
                 return
         except kazoo.exceptions.LockTimeout:
@@ -36,22 +33,17 @@ class Monitor_Backend_Handle_Worker(Abstract_Mysql_Service_Action_Thread):
         
         try:
             data_node_info_list = zkOper.retrieve_data_node_list()
-            self.__action_monitor_async(data_node_info_list)
-
-            end_time = time.time()
-            time_span = int(end_time - begin_time)
-            
-            if time_span < self.timeout-1:
-                time.sleep(self.timeout-1 - time_span)
+            self.__monitor_task_asyc(lock_name, data_node_info_list, begin_time)
 
         except Exception, e:
             self.threading_exception_queue.put(e.exc_info())
-        
+
         finally:
             if lock is not None:
                 zkOper.unLock_aysnc_monitor_action(lock)
 
-    def __action_monitor_async(self, data_node_info_list):
-        self.node_handler.retrieve_info(data_node_info_list)
-        self.db_handler.retrieve_info(data_node_info_list)
+
+    def __monitor_task_asyc(self, lock_name, data_node_info_list, time):
+        real_asyc_monitor = Monitor_handle_Asyc()
+        real_asyc_monitor._action_monitor_async(lock_name, data_node_info_list, time, timeout = self.timeout)
 
