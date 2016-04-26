cca9c98c421079d5b0536b399cc4a5d651584631
邓良驹 <dengliangju@le.com>
2016-04-26 10:53:14 +0200

    增加项目说明文件，增加打包脚本

---
+++ scripts/rpm/after_remove.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf /opt/letv/mcluster-manager
+rm -rf /opt/letv/mcluster
+rm -rf /etc/init.d/mcluster-manager
+rm -rf /etc/sysconfig/mcluster-manager
+
+exit 0
---
+++ tests/mcluster_docker/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t="letv/mcluster-manager-test:0.0.8" .
