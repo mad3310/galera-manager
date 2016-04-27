2090045e8351239de8b74a49ce0ebb0366db6717
邓良驹 <dengliangju@le.com>
2016-04-27 08:01:05 +0200

    添加rpm打包脚本

--- scripts/rpm/after_install.sh
+++ scripts/rpm/after_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cp /opt/letv/mcluster-manager/scripts/init.d/mcluster-manager_service /etc/init.d/mcluster-manager
-cp /opt/letv/mcluster-manager/scripts/init.d/mcluster-manager_conf /etc/sysconfig/mcluster-manager
 chmod +x /etc/init.d/mcluster-manager
 chkconfig --add mcluster-manager
 /etc/init.d/mcluster-manager start | stop | restart
--- scripts/rpm/after_remove.sh
+++ scripts/rpm/after_remove.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm -rf /opt/letv/mcluster-manager
-rm -rf /opt/letv/mcluster
 rm -rf /etc/init.d/mcluster-manager
 rm -rf /etc/sysconfig/mcluster-manager
 
