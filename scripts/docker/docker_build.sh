d453dfb769647e6c6d4160a106cbe6d62f13730e
gaoqiang3 <gaoqiang3@le.com>
2016-04-29 18:20:22 +0800

    添加引入基础letv-centos6镜像代码

--- scripts/docker/docker_build.sh
+++ scripts/docker/docker_build.sh
@@ -2,9 +2,12 @@
 BUILE_LOG=build.log
 VERSION=`/bin/cat version.cnf`
 
-echo ' -------------------start building letv-mcluster images----------------------' > ${BUILE_LOG}
-/usr/bin/docker import http://pkg-repo.oss.letv.com/pkgs/centos6/images/letv-centos6.tar letv:centos6
-/usr/bin/docker build -t="10.160.140.32:5002/letv-mcluster:${VERSION}" letv_mcluster_image/ >> ${BUILE_LOG}
+echo ' -------------------start import letv-centos6 images----------------------' > ${BUILE_LOG}
+docker import http://pkg-repo.oss.letv.com/pkgs/centos6/images/letv-centos6.tar letv:centos6 >> ${BUILE_LOG}
+echo ' -------------------finish import letv-centos6 images----------------------' >> ${BUILE_LOG}
+
+echo ' -------------------start building letv-mcluster images----------------------' >> ${BUILE_LOG}
+/usr/bin/docker build -t="10.160.140.32:5000/letv-mcluster:${VERSION}" letv_mcluster_image/ >> ${BUILE_LOG}
 echo ' -------------------finish building letv-mcluster images----------------------' >> ${BUILE_LOG}
 
 echo ' -------------------start building letv-mcluster-gbalancer images----------------------' >> ${BUILE_LOG}
