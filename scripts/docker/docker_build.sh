93ed7451cb6c172ab74c3dc79a919f0eeca2e3b5
邓良驹 <dengliangju@le.com>
2016-05-10 12:52:42 +0200

    更新docker镜像仓库端口，更新版本号

--- scripts/docker/docker_build.sh
+++ scripts/docker/docker_build.sh
@@ -4,17 +4,17 @@ VERSION=`/bin/cat version.cnf`
 
 echo ' -------------------start building letv-mcluster images----------------------' > ${BUILE_LOG}
 /usr/bin/docker import http://pkg-repo.oss.letv.com/pkgs/centos6/images/letv-centos6.tar letv:centos6
-/usr/bin/docker build -t="10.160.140.32:5000/letv-mcluster:${VERSION}" letv_mcluster_image/ >> ${BUILE_LOG}
+/usr/bin/docker build -t="10.160.140.32:5002/letv-mcluster:${VERSION}" letv_mcluster_image/ >> ${BUILE_LOG}
 echo ' -------------------finish building letv-mcluster images----------------------' >> ${BUILE_LOG}
 
 echo ' -------------------start building letv-mcluster-gbalancer images----------------------' >> ${BUILE_LOG}
-/usr/bin/docker build -t="10.160.140.32:5000/letv_mcluster_gbalancer:${VERSION}" mcluster_vip_gbalancer/  >> ${BUILE_LOG}
+/usr/bin/docker build -t="10.160.140.32:5002/letv_mcluster_gbalancer:${VERSION}" mcluster_vip_gbalancer/  >> ${BUILE_LOG}
 echo ' -------------------start building letv-mcluster-gbalancer images----------------------' >> ${BUILE_LOG}
 
 echo ' -------------------start push letv-mcluster images----------------------' >> ${BUILE_LOG}
-/usr/bin/docker push 10.160.140.32:5000/letv-mcluster:${VERSION}  >> ${BUILE_LOG}
+/usr/bin/docker push 10.160.140.32:5002/letv-mcluster:${VERSION}  >> ${BUILE_LOG}
 echo ' -------------------finish push letv-mcluster images----------------------' >> ${BUILE_LOG}
 
 echo ' -------------------start push letv-mcluster-gbalancer images----------------------' >> ${BUILE_LOG}
-/usr/bin/docker push 10.160.140.32:5000/letv_mcluster_gbalancer:${VERSION}  >> ${BUILE_LOG}
+/usr/bin/docker push 10.160.140.32:5002/letv_mcluster_gbalancer:${VERSION}  >> ${BUILE_LOG}
 echo ' -------------------finish push letv-mcluster-gbalancer images----------------------' >> ${BUILE_LOG}
