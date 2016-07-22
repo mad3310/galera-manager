a20a2731e6b70354e9b59bcc064a28f8cc1a7d53
denglj <dengliangju@le.com>
2016-07-22 11:33:57 +0800

    fix a bug

--- src/api/common/utils/monitor_backend_handle_worker.py
+++ src/api/common/utils/monitor_backend_handle_worker.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 import kazoo
 import time
@@ -6,34 +7,34 @@ from common.abstract_mysql_service_action_thread import Abstract_Mysql_Service_A
 from handlers.monitor import Node_Info_Async_Handler, DB_Info_Async_Handler, Check_DB_Anti_Item, Check_DB_WR_Available
 
 class Monitor_Backend_Handle_Worker(Abstract_Mysql_Service_Action_Thread):
- 
+
     node_handler = Node_Info_Async_Handler()
-    
+
     db_handler = DB_Info_Async_Handler()
-    
+
     check_db_anti_itmes = Check_DB_Anti_Item()
-    
+
     check_db_wr_available = Check_DB_WR_Available()
 
     def __init__(self, timeout, monitor_type):
         super(Monitor_Backend_Handle_Worker,self).__init__()
         self.timeout = timeout
         self.monitor_type = monitor_type
-        
+
         '''estimate unlocking zookeeper need time(secord)
         '''
         self.time_constant = 1
-        
+
     def run(self):
         '''
             if no logic below, singleton Scheduler_ZkOpers may have no self.zk object.
-        ''' 
+        '''
         begin_time = time.time()
         lock_name = 'async_monitor/' + self.monitor_type
-        
+
         zkOper = Scheduler_ZkOpers()
         logging.info('check zk is connected :%s' % str(zkOper.is_connected()))
-    
+
         isLock, lock = None, None
         try:
             isLock, lock = zkOper.lock_async_monitor_action(lock_name)
@@ -42,25 +43,25 @@ class Monitor_Backend_Handle_Worker(Abstract_Mysql_Service_Action_Thread):
         except kazoo.exceptions.LockTimeout:
             logging.info("a thread is running the monitor async, give up this oper on this machine!")
             return
-        
+
         try:
             data_node_info_list = zkOper.retrieve_data_node_list()
             getattr(self, '_async_' + self.monitor_type)(data_node_info_list)
 
             end_time = time.time()
             monitor_exc_time = int(end_time - begin_time)
-        
+
             '''leave timeout for sleep
             '''
             real_time_out = self.timeout - self.time_constant
-            
+
             if monitor_exc_time < real_time_out:
                 time.sleep(real_time_out - monitor_exc_time)
-            
+
             logging.info("%s task has finished" %self.monitor_type)
-            
+
         except Exception, e:
-            self.threading_exception_queue.put(e.exc_info())
+            self.threading_exception_queue.put(sys.exc_info())
 
         finally:
             if lock is not None:
@@ -69,9 +70,9 @@ class Monitor_Backend_Handle_Worker(Abstract_Mysql_Service_Action_Thread):
     def _async_monitor(self, data_node_info_list):
         self.node_handler.retrieve_info(data_node_info_list)
         self.db_handler.retrieve_info(data_node_info_list)
-        
+
     def _async_monitor_anti(self, data_node_info_list):
         self.check_db_anti_itmes.check(data_node_info_list)
-        
+
     def _async_monitor_write_read(self, data_node_info_list):
         self.check_db_wr_available.check(data_node_info_list)
