f66968ea30097fee1bb21e9a0c251427f6cf72e3
denglj <dengliangju@le.com>
2016-07-28 16:38:32 +0800

    修复可能会出现一直备份中的bug

--- src/api/backup_utils/backup_evl_score.py
+++ src/api/backup_utils/backup_evl_score.py
@@ -5,67 +5,45 @@ Created on 2016-01-11
 '''
 import logging
 
+
 class BackupEvlScore(object):
 
-    weight_item_score = {'memory': 10.0, 'disk': 30.0, 'load5': 30.0, 'load10': 20.0, 'load15': 10.0 }
-
-    def __init__(self):
-        self.load5_score_dict, self.load10_score_dict, self.load15_score_dict = {}, {}, {}
-        self.memory_score_dict = {}
-        self.space_score_dict = {}
-        self.host_score_dict = {}
+    item_weight = {'memory': 20.0, 'disk': 30.0, 'load1': 20.0, 'load5': 20.0, 'load15': 10.0}
 
     def _analysis_usable_backup_node(self, system_loads, free_spaces, free_memory):
-        system_loads_load5_list, system_loads_load10_list, system_loads_load15_list = [], [], []
+        # 获取各项指标的最大值，作为打分的参考标准
+        loadavg_1_max = max([float(n['loadavg_1']) for n in system_loads.itervalues()])
+        loadavg_5_max = max([float(n['loadavg_5']) for n in system_loads.itervalues()])
+        loadavg_15_max = max([float(n['loadavg_15']) for n in system_loads.itervalues()])
+        memory_max = max([float(n['MemFree']) for n in free_memory.itervalues()])
+        disk_avail_max = max([float(n['srv_mcluster_available']) for n in free_spaces.itervalues()])
 
-        memory_list = []
-        disk_dict = {}
-        disk_max_value = 0
+        # 获取各节点loadavg不同周期的分数，loadavg值越低，得分越高
+        loadavg_values_1, loadavg_values_5, loadavg_values_15 = {}, {}, {}
+        for node_ip, loadavg in system_loads.iteritems():
+            loadavg_values_1[node_ip] = self.item_weight['load1'] * (1 - float(loadavg['loadavg_1']) / loadavg_1_max)
+            loadavg_values_5[node_ip] = self.item_weight['load5'] * (1 - float(loadavg['loadavg_5']) / loadavg_5_max)
+            loadavg_values_15[node_ip] = self.item_weight['load15'] * (1 - float(loadavg['loadavg_15']) / loadavg_15_max)
 
-        [system_loads_load5_list.append(float(system_loads[i]['loadavg_5'])) for i in system_loads]
-        [system_loads_load10_list.append(float(system_loads[i]['loadavg_10'])) for i in system_loads]
-        [system_loads_load15_list.append(float(system_loads[i]['loadavg_15'])) for i in system_loads]
-        [memory_list.append(float(free_memory[i]['MemFree'])) for i in free_memory]
+        # 获取各节点剩余内存的分数，可用内存越大，得分越高
+        memory_values = {}
+        for node_ip, meminfo in free_memory:
+            memory_values[node_ip] = self.item_weight['memory'] * float(meminfo['MemFree']) / memory_max
 
-        for k in free_spaces:
-            total = float(free_spaces[k]['srv_mcluster_total'])
-            avail = float(free_spaces[k]['srv_mcluster_available'])
-            data_avail = float(free_spaces[k]['data_directory_available'])
+        # 获取各节点剩余磁盘容量的分数，容量越大，得分越高
+        disk_values = {}
+        for node_ip, diskinfo in free_spaces:
+            disk_values[node_ip] = self.item_weight['disk'] * float(diskinfo['srv_mcluster_available']) / disk_avail_max
 
-            if avail > (total-avail):
-                disk_dict[k] = avail
-                if avail > disk_max_value:
-                    disk_max_value = avail
-
-        for i in system_loads:
-            self.load5_score_dict[i] = self.weight_item_score['load5'] * max(system_loads_load5_list)/(float(system_loads[i]['loadavg_5'])+0.000001)
-            self.load10_score_dict[i] = self.weight_item_score['load10'] * max(system_loads_load10_list)/(float(system_loads[i]['loadavg_10'])+0.000001)
-            self.load15_score_dict[i] = self.weight_item_score['load15'] * max(system_loads_load15_list)/(float(system_loads[i]['loadavg_15'])+0.000001)
-
-        for j in free_memory:
-            self.memory_score_dict[j] = (self.weight_item_score['memory']*float(free_memory[j]['MemFree']))/max(memory_list)
-
-        for k in disk_dict:
-            if disk_max_value:
-                self.space_score_dict[k] = (self.weight_item_score['disk']*disk_dict[k])/disk_max_value
-
-        self.__get_item_score()
-
-        return self._get_backup_node_ip()
-
-
-    def _get_backup_node_ip(self):
-        host_score_list = sorted(self.host_score_dict.items(), key=lambda i: i[1], reverse=True)
-        host_list = [i[0] for i in host_score_list]
-
-        logging.info('select host list :%s' % str(host_list))
-        return host_list
-
-    def __get_item_score(self):
-        for _score_ip in self.space_score_dict:
-            if self.space_score_dict[_score_ip]:
-                self.host_score_dict[_score_ip] = self.memory_score_dict[_score_ip] + self.space_score_dict.get(_score_ip) \
-                + self.load5_score_dict[_score_ip] + self.load10_score_dict[_score_ip] + self.load15_score_dict[_score_ip]
-
-            else:
-                logging.info("The disk is full")
+        # 计算每个节点的总分
+        nodes_score = {}
+        for node_ip in disk_values:
+            nodes_score[node_ip] = sum([loadavg_values_1[node_ip],
+                                        loadavg_values_5[node_ip],
+                                        loadavg_values_15[node_ip],
+                                        memory_values[node_ip],
+                                        disk_values[node_ip]])
+        # 按总分倒序排列后的节点IP列表
+        nodes_sorted_by_score = sorted(nodes_score, key=nodes_score.get, reverse=True)
+        logging.info('selected backup nodes:%s' % str(nodes_sorted_by_score))
+        return nodes_sorted_by_score
