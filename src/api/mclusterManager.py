acb764733d09ad30ebeaf1f2756b6c677d97c333
chenwenquan <chenwenquan@le.com>
2016-10-19 10:40:19 +0800

    修改tornado项目启动文件

--- src/api/mclusterManager.py
+++ src/api/mclusterManager.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-import os.path
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
-
 from tornado.options import options
-from common.appdefine import mclusterManagerDefine
+
+from common.appdefine import mclusterManagerDefine  #flake8: noqa
 from common.scheduler_opers import Scheduler_Opers
+
 import routes
 
 
 class Application(tornado.web.Application):
-    def __init__(self):
 
+    def __init__(self):
         settings = dict(
-            template_path=os.path.join(options.base_dir, "templates"),
-            ui_modules={"Entry": None},
-            xsrf_cookies=False,
-            cookie_secret="16oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp5XdTP1o/Vo=",
-            login_url="/auth/login",
             debug=options.debug,
         )
-
         tornado.web.Application.__init__(self, routes.handlers, **settings)
 
 
