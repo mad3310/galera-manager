2e417f248b82d6c60ecfb22051ca16b696a21980
chenwenquan <chenwenquan@le.com>
2016-10-22 01:10:02 +0800

    添加常量定义

--- src/api/common/consts.py
+++ src/api/common/consts.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from src.api.utils.storify import storify
+
 MONITOR_TYPE = {'db': ['existed_db_anti_item',
                        'wsrep_status',
                        'cur_user_conns',
@@ -12,3 +14,9 @@ MONITOR_TYPE = {'db': ['existed_db_anti_item',
                          'started'
                          ]
                 }
+
+ALARM_LEVEL = storify(dict(
+    SERIOUS='tel:sms:email',
+    GENERAL='sms:email',
+    NOTHING='nothing'
+))
