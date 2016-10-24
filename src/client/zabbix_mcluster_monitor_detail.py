cb6bf7a357e7dadfd3fc71821f269b68f19ebfd0
chenwenquan <chenwenquan@le.com>
2016-10-24 22:30:14 +0800

    潜在BUG:解决循环依赖问题

--- src/client/zabbix_mcluster_monitor_detail.py
+++ src/client/zabbix_mcluster_monitor_detail.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 '''
 Created on 2013-7-21
@@ -13,19 +13,20 @@ import time
 
 MCLUSTER_VIP = 'localhost'
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
-WARNING_TIME_DIFF = 210
+WARNING_TIME_DIFF = 250
+
 
 def check_monitor_detail(serious_dict, general_dict, nothing_dict):
     url = "http://%s:8888/mcluster/status" % (MCLUSTER_VIP)
     f = urllib2.urlopen(url)
-    encodedjson =  f.read()
+    encodedjson = f.read()
     monitor_return_json_value = json.loads(encodedjson)
-    
+
     reponse_code = monitor_return_json_value['meta']['code']
     if reponse_code != 200:
         serious_dict.setdefault(url, "due to response code error, please check email to find the reason!")
         return
-    
+
     for monitor_type in monitor_return_json_value['response']:
         for monitor_item in monitor_return_json_value['response'][monitor_type]:
             monitor_type_item = monitor_type + "." + monitor_item
@@ -37,33 +38,33 @@ def check_monitor_detail(serious_dict, general_dict, nothing_dict):
                 return_message += str(error_record)
             create_time_str = monitor_return_json_value['response'][monitor_type][monitor_item]['ctime']
             t = time.strptime(create_time_str, TIME_FORMAT)
-            create_time = datetime.datetime(*t[:6]) 
+            create_time = datetime.datetime(*t[:6])
 #            print str(create_time)
-            
+
             check_time = datetime.datetime.now()
 #            print str(check_time)
-            diff = (check_time-create_time).seconds
+            diff = (check_time - create_time).seconds
 #            print diff
 
             if diff > WARNING_TIME_DIFF:
-                serious_dict.setdefault(monitor_type_item,"please check async api, the data is out of date!")
+                serious_dict.setdefault(monitor_type_item, "please check async api, the data is out of date!")
             elif return_code == 'tel:sms:email':
-                serious_dict.setdefault(monitor_type_item,return_message)
+                serious_dict.setdefault(monitor_type_item, return_message)
             elif return_code == 'sms:email':
-                general_dict.setdefault(monitor_type_item,return_message)
+                general_dict.setdefault(monitor_type_item, return_message)
             else:
-                nothing_dict.setdefault(monitor_type_item,return_message)
-    
-    
+                nothing_dict.setdefault(monitor_type_item, return_message)
+
+
 def main():
     serious_dict = {}
-    
+
     general_dict = {}
-    
+
     nothing_dict = {}
-    
+
     check_monitor_detail(serious_dict, general_dict, nothing_dict)
-    
+
     print "detail_serious:%s" % serious_dict
     print "detail_general:%s" % general_dict
     print "detail_nothing:%s" % nothing_dict
