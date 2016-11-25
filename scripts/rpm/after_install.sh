d487d0a349ca53163d9a04f8bce6a6b84c23ebf4
chenwenquan <chenwenquan@le.com>
2016-11-25 13:27:01 +0800

    删除多余的simplejson依赖

--- scripts/rpm/after_install.sh
+++ scripts/rpm/after_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 删除多余的simplejson依赖
+rm -rf /usr/lib64/python2.6/site-packages/simplejson
+rm -rf /usr/lib64/python2.6/site-packages/simplejson-2.0.9-py2.6.egg-info
+
 chmod +x /etc/init.d/mcluster-manager
 chkconfig --add mcluster-manager
 /etc/init.d/mcluster-manager start | stop | restart
